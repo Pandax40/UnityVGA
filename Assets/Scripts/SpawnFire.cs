5b0966fc295a3fea32889fd85bc61cdf215676c3
marrqqss <marcribasacon@gmail.com>
2023-07-03 21:13:27 +0200

    Fireball and Fireball Spawner

--- Assets/Scripts/SpawnFire.cs
+++ Assets/Scripts/SpawnFire.cs
@@ -13,27 +13,33 @@ public class SpawnFire : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        timer = 40f;
+        timer = 100f;
+        auxiliartimer = 0.0f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        float randomx = Random.Range(RangoMenosX,RangoX);
-        Vector3 vec = new Vector3(randomx, 0, 0);
-        FireSpawn(vec);
+        if (auxiliartimer <= 0)
+        {
+            float randomx = Random.Range(RangoMenosX, RangoX);
+            Vector3 vec = new Vector3(randomx, 0, 0);
+            FireSpawn(vec);
+            auxiliartimer = 0.5f;
+        }
     }
 
     private void FixedUpdate()
     {
         --timer;
         if (timer <= 0) Destroy(this);
+        --auxiliartimer;
     }
 
     public void FireSpawn(Vector3 pos)
     {
         GameObject Bola = Instantiate(FireBall, transform.position + pos, Quaternion.identity);
         Bola.transform.eulerAngles = new Vector3(0, 0, 270);
-        Destroy(Bola, 5f);
+        Destroy(Bola, 1f);
     }
 }
