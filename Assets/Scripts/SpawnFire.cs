e28b2995fe5a6068879a859a90deb76c50095f3a
marrqqss <marcribasacon@gmail.com>
2023-07-03 21:01:53 +0200

    Idk algunos sprites

---
+++ Assets/Scripts/SpawnFire.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnFire : MonoBehaviour
+{
+    public GameObject FireBall;
+    private float timer;
+    private float auxiliartimer;
+    [SerializeField] private float RangoX;
+    [SerializeField] private float RangoMenosX;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = 40f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float randomx = Random.Range(RangoMenosX,RangoX);
+        Vector3 vec = new Vector3(randomx, 0, 0);
+        FireSpawn(vec);
+    }
+
+    private void FixedUpdate()
+    {
+        --timer;
+        if (timer <= 0) Destroy(this);
+    }
+
+    public void FireSpawn(Vector3 pos)
+    {
+        GameObject Bola = Instantiate(FireBall, transform.position + pos, Quaternion.identity);
+        Bola.transform.eulerAngles = new Vector3(0, 0, 270);
+        Destroy(Bola, 5f);
+    }
+}
